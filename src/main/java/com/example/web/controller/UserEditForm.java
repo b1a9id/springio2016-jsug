21db485e616c03a5a24ea104c76a949a33ada5ee
内立良介 <uchitate@tagbangers.co.jp>
2016-06-16 19:08:02 +0900

    change @NotNull to @NotEmpty

--- src/main/java/com/example/web/controller/UserEditForm.java
+++ src/main/java/com/example/web/controller/UserEditForm.java
@@ -5,6 +5,7 @@ import com.example.core.model.UserUpdateRequest;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -13,7 +14,7 @@ import java.io.Serializable;
 @Setter
 public class UserEditForm implements Serializable {
 
-	@NotNull
+	@NotEmpty
 	private String name;
 
 	@Email
